d59abd4c75de8376650242022b9fdd293237b30b
castamonte <castamonte@gmail.com>
2012-01-27 16:26:19 +0400

    сделал OnEditorActionListener

--- org.no-ip.tooman/src/org/dyndns/tooman/t_adjust.java
+++ org.no-ip.tooman/src/org/dyndns/tooman/t_adjust.java
@@ -2,56 +2,56 @@ package org.dyndns.tooman;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class t_adjust extends Activity implements OnClickListener {
+public class t_adjust extends Activity {
 	private EditText ar0, t0, t1;
 	private TextView ar1, ro0, ro1;
 	private Integer i_ar0, i;
-	@Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.t_adjust);
-        
-        ar0 = (EditText)findViewById(R.id.editText1);
-/* пока без обработки изменения поля
-        ar0.addTextChangedListener(new TextWatcher(){
-        	public void afterTextChanged(Editable s) {}
-            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
-            public void onTextChanged(CharSequence s, int start, int before, int count){}
-        });
-*/
-        t0 = (EditText)findViewById(R.id.editText2);
-        t1 = (EditText)findViewById(R.id.editText3);
-        ar1 = (TextView)findViewById(R.id.textView5);
-        ro0 = (TextView)findViewById(R.id.textView7);
-        ro1 = (TextView)findViewById(R.id.textView9);
-        i=0;
-    }
-
-    @Override
-    public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.button1:
-	    	recalc();
-// больше кнопок нету
+	TextView.OnEditorActionListener etListener = new TextView.OnEditorActionListener() {
+		@Override
+		public boolean onEditorAction(TextView exampleView, int actionId,KeyEvent event) {
+			if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_NEXT) {
+				recalc();
+			}
+			return false;
 		}
+	};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.t_adjust);
+
+		ar1 = (TextView) findViewById(R.id.textView5);
+		ro0 = (TextView) findViewById(R.id.textView7);
+		ro1 = (TextView) findViewById(R.id.textView9);
+
+		ar0 = (EditText) findViewById(R.id.editText1);
+		ar0.setOnEditorActionListener(etListener);
+
+		t0 = (EditText) findViewById(R.id.editText2);
+		t0.setOnEditorActionListener(etListener);
+		
+		t1 = (EditText) findViewById(R.id.editText3);
+		t1.setOnEditorActionListener(etListener);
+		
+		i = 0;
 	}
-        
-    private void recalc()
-    {
-    	i_ar0 = Integer.valueOf(ar0.toString());
-//    	i_t0 = Integer.parseInt(t0.toString());
-//    	i_t1 = Integer.parseInt(t1.toString());
-//    	ar1.setText(String.valueOf(i_ar0));
-/*
-    	i++;
-    	ar1.setText(String.valueOf(i));
-*/
-    }
+
+	private void recalc() {
+		Log.i("KeyBoard", "recalc()");
+		// i_ar0 = Integer.valueOf(ar0.toString());
+		// i_t0 = Integer.parseInt(t0.toString());
+		// i_t1 = Integer.parseInt(t1.toString());
+		// ar1.setText(String.valueOf(i_ar0));
+		/*
+		 * i++; ar1.setText(String.valueOf(i));
+		 */
+	}
 
 }
