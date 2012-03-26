bd00e4919c39540c79fe4afd973db2b281c50f05
castamonte <castamonte@gmail.com>
2012-03-26 11:35:02 +0400

    заработал запуск активити из листвью

--- org.no-ip.tooman/src/org/dyndns/tooman/hello.java
+++ org.no-ip.tooman/src/org/dyndns/tooman/hello.java
@@ -3,17 +3,18 @@ package org.dyndns.tooman;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class hello extends Activity {
 
@@ -23,8 +24,8 @@ public class hello extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hello);
-		ListView myListView = (ListView) findViewById(R.id.listView1);
 
+		ListView myListView = (ListView) findViewById(R.id.listView1);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
 				this, R.array.works, android.R.layout.simple_list_item_1);
 		myListView.setAdapter(adapter);
@@ -34,6 +35,14 @@ public class hello extends Activity {
 					int position, long id) {
 				Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
 						+ id);
+				Intent myIntent = null;
+
+				if (position == 0) {
+					myIntent = new Intent(view.getContext(), t_adjust.class);
+				}
+
+				startActivity(myIntent);
+
 			}
 		});
 
