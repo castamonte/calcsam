2c1cbcde71ef64e06a97a7696fc7225b329bfec5
castamonte <castamonte@gmail.com>
2012-04-20 16:31:44 +0400

    немного формул

--- org.no-ip.tooman/src/org/dyndns/tooman/distillation.java
+++ org.no-ip.tooman/src/org/dyndns/tooman/distillation.java
@@ -55,18 +55,73 @@ public class distillation extends Activity {
 		}
 		d_s0 = ttos(i_t0);
 		d_s_end = ttos(i_t_end);
-		Log.i("s0", String.format("%.1f", d_s0));
-		Log.i("s_end", String.format("%.1f", d_s_end));
-		s0.setText(String.format("%.1f", d_s0));
-		s_end.setText(String.format("%.1f", d_s_end));
+		Log.i("s0", String.format("%.2f", d_s0));
+		Log.i("s_end", String.format("%.2f", d_s_end));
+		s0.setText(String.format("%.0f", d_s0));
+		s_end.setText(String.format("%.0f", d_s_end));
 	}
 
-	protected double ttos(int spirit) {
+	protected double ttos(int temp) {
 		double x;
+// зависимость градуса от температуры кипения
 		x = 1.340861289354e+04 
-			- 4.209916659595e+02 * spirit
-		    + 4.421702244267e+00 * spirit*spirit
-		    - 1.552843491420e-02 * spirit*spirit*spirit;
+			- 4.209916659595e+02 * temp
+		    + 4.421702244267e+00 * temp*temp
+		    - 1.552843491420e-02 * temp*temp*temp;
 		return x;		
 	}
+	
+	protected double ttom(int temp) {
+		double x;
+// зависимость содержания спирта в парах (% молярный) от температуры кипения		
+		x = 1.554571860871e+06
+		    - 8.712650943627e+04 * temp
+		    + 1.950864596202e+03 * temp * temp
+		    - 2.181278765435e+01 * temp * temp * temp
+		    + 1.217832635022e-01 * temp * temp * temp * temp
+		    - 2.716106516360e-04 * temp * temp * temp * temp * temp;
+		return x;
+	}
+	
+	protected double mtot(double mp) {
+		double x;
+// зависимость температуры кипения от %(молярного) спирта в баке
+		x = 9.973680403705e+01
+			- 2.664647907634e+00 * mp
+			+ 1.995928679785e-01 * mp * mp
+			- 8.594968601874e-03 * mp * mp * mp
+			+ 2.122481550887e-04 * mp * mp * mp * mp
+			- 2.961745380649e-06 * mp * mp * mp * mp * mp
+			+ 2.164433737491e-08 * mp * mp * mp * mp * mp * mp
+			- 6.424471873789e-11 * mp * mp * mp * mp * mp * mp * mp;
+/*		x = 2252.8*(mp*mp*mp*mp*mp*mp)-6621.1*(mp*mp*mp*mp*mp)
+			+7684*(mp*mp*mp*mp)-4493.3*(mp*mp*mp)
+			+1404.7*(mp*mp)-236.65*mp+100;*/
+		return x;
+	}
+	
+	protected double mtom(double mp) {
+		double x;
+// зависимость % мол. в паре от % мол. в баке
+		x = 1.234589531895e+00
+			+ 9.285042487341e+00 * mp
+			- 7.601202637658e-01 * mp * mp
+			+ 3.444521563141e-02 * mp * mp * mp
+			- 8.774226026331e-04 * mp * mp * mp * mp
+			+ 1.252947859514e-05 * mp * mp * mp * mp * mp
+			- 9.325991073677e-08 * mp * mp * mp * mp * mp * mp
+			+ 2.811389881792e-10 * mp * mp * mp * mp * mp * mp * mp;
+		return x;
+	}
+	
+	protected void disteps() {
+		double de=0.789, dw=1, mwe=46.0634, mww=18.0152,
+				cpe=2.845, cpw=4.184, hve=761, hvw=2137;
+		double ml_e, gr_e, mole_e, ml_w, gr_w, mole_w, mp_e;
+// первая итерация
+		ml_e = i_amount * d_s0 / 100; gr_e = ml_e * de; mole_e = gr_e / mwe;
+		ml_w = i_amount - ml_e; gr_w = ml_w; mole_w = gr_w / mww;
+		
+	}
+
 }
