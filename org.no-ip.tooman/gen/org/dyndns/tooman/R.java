4f469622c79cf42fd5970a78c76a3826317fb5ba
castamonte <castamonte@gmail.com>
2012-09-21 16:16:07 +0400

    лого без тени

--- org.no-ip.tooman/gen/org/dyndns/tooman/R.java
+++ org.no-ip.tooman/gen/org/dyndns/tooman/R.java
@@ -22,10 +22,11 @@ public final class R {
         public static final int myColor=0x7f060000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int logo_hd=0x7f020003;
+        public static final int hd=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int logo_hd=0x7f020004;
     }
     public static final class id {
         public static final int button1=0x7f090021;
